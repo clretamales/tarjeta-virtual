5baf334d519a0fec3ca08c890acdb947c8586bb5
Jose Andres Retamales <joseandres.retamales@gmail.com>
2026-03-11 11:04:27 -0300

    correciones parametros

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,20 @@
-export default {
-  languageOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    globals: {
-      window: "readonly",
-      document: "readonly"
+import eslintRecommended from "eslint/conf/eslint-recommended";
+
+export default [
+  eslintRecommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": ["error", { vars: "all", args: "none", ignoreRestSiblings: true }],
+      "no-console": "error",
+      "prefer-const": ["warn"]
     }
-  },
-  extends: ["eslint:recommended"],
-  rules: {
-    "no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": true }],
-    "no-console": "error",
-    "prefer-const": ["warn"]
   }
-};
+];
