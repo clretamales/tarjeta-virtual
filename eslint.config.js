ecb08d9e26231436722c7311f9e88639fbc5083d
Jose Andres Retamales <joseandres.retamales@gmail.com>
2026-03-11 11:07:18 -0300

    fix: simplify eslint flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,15 @@
-import eslintRecommended from "eslint/conf/eslint-recommended";
-
-export default [
-  eslintRecommended,
-  {
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        window: "readonly",
-        document: "readonly"
-      }
-    },
-    rules: {
-      "no-unused-vars": ["error", { vars: "all", args: "none", ignoreRestSiblings: true }],
-      "no-console": "error",
-      "prefer-const": ["warn"]
+export default {
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    globals: {
+      window: "readonly",
+      document: "readonly"
     }
+  },
+  rules: {
+    "no-unused-vars": ["error", { vars: "all", args: "none", ignoreRestSiblings: true }],
+    "no-console": "error",
+    "prefer-const": ["warn"]
   }
-];
+};
